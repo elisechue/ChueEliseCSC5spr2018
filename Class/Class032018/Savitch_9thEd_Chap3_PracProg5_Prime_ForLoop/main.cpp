0e3b1c96cc270a1cbb32635ff0732cf0248c599b
elisechue <echue1@student.rccd.edu>
2018-03-20 11:07:30 -0700

    Signed-off-by: elisechue <echue1@student.rccd.edu>

--- Class/Class032018/Savitch_9thEd_Chap3_PracProg5_Prime_ForLoop/main.cpp
+++ Class/Class032018/Savitch_9thEd_Chap3_PracProg5_Prime_ForLoop/main.cpp
@@ -23,25 +23,25 @@ using namespace std;
 
 int main(int argc, char** argv) {
     //set the random number seed
-    srand(static_cast<unsigned int>(time(0)));
+    //srand(static_cast<unsigned int>(time(0)));
     
     //Declare Variables
-    int nLoops=100;
-    bool prime;     //determine if num2Tst is prime
+   // int nLoops=100;
+   // bool prime;     //determine if num2Tst is prime
     
     //Initialize Variables
     
     //Calculate the answer
-    for(int num2Tst=2;num2Tst<=100;num2Tst++){
-        prime=true;
-    for(int i=2;1<=sqrt(nLoops);i++){
+    //for(int num2Tst=2;num2Tst<=100;num2Tst++){
+       // prime=true;
+    //for(int i=2;1<=sqrt(nLoops);i++){
         prime=num2Tst==i?true&&prime:prime&&num2Tst%i;
     }
          //Display Outputs
-    cout<<static_cast<int>(num2Tst)
-            <<" is "
-            <<(prime?"Prime":"Not Prime")
-            <<endl;
+    //cout<<static_cast<int>(num2Tst)
+          //  <<" is "
+          //  <<(prime?"Prime":"Not Prime")
+          //  <<endl;
   }
    
     //Exit Program!
