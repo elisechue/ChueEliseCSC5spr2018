2ee51a35b1066a71a0aa796e6c4b2dbf5bec2f96
elisechue <echue1@student.rccd.edu>
2018-05-03 12:46:49 -0700

    Signed-off-by: elisechue <echue1@student.rccd.edu>

--- Lab/Lab050318/RetirementFunction/nbproject/Makefile-variables.mk
+++ Lab/Lab050318/RetirementFunction/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=cpptemplate
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cpptemplate
+CND_ARTIFACT_NAME_Debug=retirementfunction
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/retirementfunction
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=cpptemplate.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cpptemplate.tar
+CND_PACKAGE_NAME_Debug=retirementfunction.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/retirementfunction.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=cpptemplate
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/cpptemplate
+CND_ARTIFACT_NAME_Release=retirementfunction
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/retirementfunction
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=cpptemplate.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/cpptemplate.tar
+CND_PACKAGE_NAME_Release=retirementfunction.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/retirementfunction.tar
 #
 # include compiler specific variables
 #
