dff8276dbf79319bb324385265133958ba69985d
elisechue <echue1@student.rccd.edu>
2018-05-31 10:51:43 -0700

    Signed-off-by: elisechue <echue1@student.rccd.edu>

---
+++ Class/Class053118/Dynamic_1and2D_Arrays_with_Structures/Array1D.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Elise Chue
+ * Created on May 31, 2018, 10:35 AM
+ * Purpose:  Specification for the 1-D Array Structure
+ */
+
+#ifndef ARRAY1D_H
+#define ARRAY1D_H
+
+struct Array1D{
+    int size;   //size of 1D array
+    int *data; //pointer to dynamic array
+};
+
+
+#endif /* ARRAY1D_H */
+
---
+++ Class/Class053118/Dynamic_1and2D_Arrays_with_Structures/main.cpp
@@ -0,0 +1,107 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr Mark E. Lehr, Elise Chue
+ * Created on May 31, 2018, 10:35 AM
+ * Purpose:  Dynamic 1 and 2 D Arrays with STRUCTURES
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+#include "Array1D.h"
+#include "Array2D.h"
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+Array1D *fillAry(int);
+void prntAry(Array1D *,int);
+Array2D *fillAry(int,int);
+void prntAry(Array2D *);
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Declare Variables
+    int size=100;
+    int rows=10;
+    int cols=10;
+    
+    //Initialize Variables
+    Array1D *array=fillAry(size);
+    int **array2=fillAry(rows,cols);
+    
+    //Print the Array
+    prntAry(array,10);
+    prntAry(array2,rows,cols);
+    
+    //Clean up
+    delete []array->data;   //data within the structure is the array
+    delete array;           //structure 
+    //De-Allocate columns
+    for(int row=0;row<rows;row++){
+        delete []array2[row];
+    }
+    //De-Allocate the rows
+    delete []array2;
+    
+    //Exit stage right!
+    return 0;
+}
+
+void prntAry(int **a,int rows,int cols){
+    cout<<endl;
+    for(int row=0;row<rows;row++){
+        for(int col=0;col<cols;col++){
+            cout<<a[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+int **fillAry(int rows,int cols){
+    //Allocate rows
+    int **a=new int*[rows];
+    //Allocate columns
+    for(int row=0;row<rows;row++){
+        a[row]=new int[cols];
+    }
+    //Fill the array
+    for(int row=0;row<rows;row++){
+        for(int col=0;col<cols;col++){
+            a[row][col]=rand()%90+10;
+        }
+    }
+    return a;
+}
+
+void prntAry(Array1D *a,int perLine){
+    cout<<endl;
+    for(int i=0;i<a->size;i++){
+        cout<<a->data[i]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+Array1D *fillAry(int n){
+    //Allocate memory
+    Array1D *a=new Array1D;     //cant just point to it gotta allocate memory
+    a->size=n;
+    a->data=new int[a->size];
+    //Fill the array
+    for(int i=0;i<n;i++){
+        a->data[i]=rand()%90+10;//[10,99]
+    }
+    return a;
+}
+
+//remember to debug one step/section at a time. oftentimes the error is not gonna be informative
+//difference between dot and arrow. dot is for regular variables. arrow is for pointers
